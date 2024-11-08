41cd76f2f6ae5085fcd396a945fbdd21658cdb2a
Mishywayu <michellewayua2@gmail.com>
2024-11-08 14:51:07 +0300

    ITERATORS and ITERABLES

---
+++ Iterators.py
@@ -0,0 +1,6 @@
+# ITERATORS and ITERABLES
+# ITERATOR - is an object that implements the iterator protocol, 
+#             which consists of the methods __iter__() and __next__().
+# ITERABLE - is any object that can return its members one at a time, 
+#            allowing it to be iterated over in a loop. Examples of iterables include 
+#            lists, tuples, dictionaries, sets, and strings.
